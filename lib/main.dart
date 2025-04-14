e82fc37180e4a690bd4dc0414e65c94b041889be
rostom17 <rostom825@gmail.com>
2025-04-14 15:12:13 +0600

    Firabase Setup Successfull

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:farmers_nest/app.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:farmers_nest/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(FarmersNest());
 }
